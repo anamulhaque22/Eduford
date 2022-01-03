5349133adb7cecdbba90083ddba9dc838ba06218
Anamul Haque <anamulah535367@gmail.com>
2022-01-03 08:00:55 +0600

    Eduford Project

---
+++ js/main.js
@@ -0,0 +1,8 @@
+/*TOGGLE MENU*/
+var navLink = document.getElementById("navLinks")
+function openMenu(){
+    navLink.style.right = "0";
+}
+function closeMenu(){
+    navLink.style.right= "-200px"
+}
